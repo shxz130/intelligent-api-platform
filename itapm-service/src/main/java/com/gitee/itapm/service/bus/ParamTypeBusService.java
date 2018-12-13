33a8f6bec9e772e23f5de7ce92b221428642f823
jetty <473816910@qq.com>
2018-12-13 21:56:02 +0800

    document解析

--- itapm-service/src/main/java/com/gitee/itapm/service/bus/ParamTypeBusService.java
+++ itapm-service/src/main/java/com/gitee/itapm/service/bus/ParamTypeBusService.java
@@ -3,10 +3,12 @@ package com.gitee.itapm.service.bus;
 import com.gitee.itapm.service.ParamTypeService;
 import com.gitee.itapm.service.bean.ParamTypeBO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by jetty on 2018/12/13.
  */
+@Component
 public class ParamTypeBusService {
 
     @Autowired
